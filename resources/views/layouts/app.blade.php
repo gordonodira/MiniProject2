7ffb789f1106bcba291889f5c59ed323c1acc9f9
gordonodira <go37@njit.edu>
2018-11-01 15:46:19 -0400

    Number 6 Aand 7: About and Contact Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,8 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('contact') }}">Contact</a>
+                                    <a class="dropdown-item" href="{{ route('about') }}">About</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf

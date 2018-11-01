421bdc0063492952e934afe095836ed280726d52
gordonodira <go37@njit.edu>
2018-11-01 16:16:49 -0400

    TESTS: Success CONTACT Page Test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+//{
     /**
      * Create a new controller instance.
      *
      * @return void
      */
     public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+{
+    //{
+     $this->middleware('auth');
+    // }
+}
     /**
      * Show the application dashboard.
      *

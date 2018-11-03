9a9f3fc2a3fe275e2dc686e3a1e11335789aa78f
gordonodira <go37@njit.edu>
2018-11-02 22:07:46 -0400

    TESTS: successful INSERT CAR unit testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Unit;
 
+use App\Car;
 use PHPUnit\Framework\Constraint\IsType;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+
 
 class ExampleTest extends TestCase
 {
@@ -43,5 +45,13 @@ class ExampleTest extends TestCase
         $this->assertInternalType( IsType::TYPE_INT,$recordCount);
           $recordCount=$users->count();
          }
+    public function testCarTest(){
 
+        $car=new Car();
+        $car->make='Toyota';
+        $car->model='CARC';
+        $car ->year='1999';
+
+        $this->assertTrue($car->save());
+       }
     }
